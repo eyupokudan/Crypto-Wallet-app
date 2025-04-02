7928fcfcb022d5cd9f3f2e57b059200af22a7553
eyupoudan <eyupensarokudan34@gmail.com>
2025-04-02 22:34:15 +0100

    Defined global variables and placeholders for load/save functions

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ ctk.set_default_color_theme("dark-blue")
 app = ctk.CTk()
 app.title("Crypto Wallet")
 app.geometry("1100x900")
+portfolio = []
+coin_list = []
+selected_coin = {}
+sort_option = tk.StringVar(value="name")
+
+def load_portfolio():
+    pass
+
+def save_portfolio():
+    pass
 
 app.mainloop()
 
