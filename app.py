c20d152f7136fd07bf1360285cb3eabf1fa7ba6e
eyupoudan <eyupensarokudan34@gmail.com>
2025-04-02 22:31:13 +0100

    Created app window using customtkinter

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import customtkinter as ctk
+import tkinter as tk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+app = ctk.CTk()
+app.title("Crypto Wallet")
+app.geometry("1100x900")
+
+app.mainloop()
 
